ee7d6a7642b6f1891faf48d78901ad3b221e86d8
John Shiver <johneshiver@gmail.com>
2014-06-29 16:34:32 -0700

    Updated ajax to work on edits.  Still MANY bugs

--- static/ajax.js
+++ static/ajax.js
@@ -12,3 +12,18 @@ $(document).ready(function() {
     });
   });
 });
+
+$(document).ready(function() {
+  $('.edit_entry').on('submit', function(event) {
+    event.preventDefault();
+    $.ajax('/update/{{ entry[0] }}', {
+      type: 'POST',
+      data: $('form').serialize(),
+      success: function(data) {
+        $('.new').html(data);
+        $('.add_entry').remove();
+
+      }
+    });
+  });
+});
