d452d857a727121f7d791db84bf0384924120fdf
John Shiver <johneshiver@gmail.com>
2015-03-15 21:10:51 -0700

    Syntax error

--- twitter.py
+++ twitter.py
@@ -4,7 +4,6 @@ from dateutil import parser
 import os
 
 
-
 consumer_key = os.environ.get('consumer_key', None)
 consumer_secret = os.environ.get('consumer_secret', None)
 access_token = os.environ.get('access_token', None)
